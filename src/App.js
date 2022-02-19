2ded48408b91dba8e71ddcff93bdf516bf3dc35a
Isaac De La Cruz <89318065+Sirkickassalot@users.noreply.github.com>
2022-02-19 12:27:47 -0800

    added useState variable of count

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import './App.css'
+import { useState } from 'react'
 
 // want to make a COUNTER app
 // 1. Make a button
 // 2. detect when the button is clicked
 
+//  state
+
 
 function App () {
+  const [count, setCount] = useState(20)
+  // increment a state variable >>> setCount(count + 1)
+
   let myAge = 20
+  // increment a REGULAR variable >>> myAge = myAge + 1
   return (
     <>
-      <h2 style={{color: 'red'}}>My Name is Isaac, I am {myAge} years old.</h2>
-      <button onClick={() => {
-        myAge = myAge + 1
-        alert(myAge)
-      }}> + </button>
-      <button onClick={() => alert('you clicked the - button')}> - </button>
+      <h2 style={{color: 'red'}}>{count}</h2>
+      <button onClick={() => {setCount(count + 1)}}> + </button>
+      <button onClick={() => {setCount(count - 1)}}> - </button>
     </>
   )
 
 }
 
+// (in simplest terms) if you use a state variable instead of a regular variable; the state variable gets updated in real time.
 // The empty divs are JSX Fragments
 // How to make a funcitonal component
 // variables
